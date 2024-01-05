4937d555aea94d9720e1d3c67d6a2d25a37e30b4
Paulo Ricardo <pr741234@gmail.com>
2024-01-05 15:55:48 -0300

    :tada: first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Primeiro Valor: ");
+            float num1 = float.Parse(Console.ReadLine());
+            Console.WriteLine(num1);
+        }
+    }
+}
