0ac02717458a831bd28222e2d18b9e7424d8473e
Paulo Ricardo <pr741234@gmail.com>
2024-01-05 17:48:12 -0300

    adicionado operacao divisao e subtracao

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
   {
     static void Main(string[] args)
     {
-      Addition();
+      Division();
     }
 
     static void Addition()
@@ -12,11 +12,43 @@
       Console.Clear();
       Console.WriteLine("Primeiro Valor:");
       float firstOperator = float.Parse(Console.ReadLine());
+
       Console.WriteLine("Segundo Valor:");
       float secondOperator = float.Parse(Console.ReadLine());
+
       Console.Clear();
       float result = firstOperator + secondOperator;
+      Console.WriteLine($"O resultado da subtração é: {result}");
+    }
+
+    static void Subtraction()
+    {
+      Console.Clear();
+      Console.WriteLine("Primeiro Valor:");
+      float firstOperator = float.Parse(Console.ReadLine());
+
+      Console.WriteLine("Segundo Valor:");
+      float secondOperator = float.Parse(Console.ReadLine());
+
+      Console.Clear();
+      float result = firstOperator - secondOperator;
       Console.WriteLine($"O resultado da soma é: {result}");
+      Console.ReadKey();
+    }
+
+    static void Division()
+    {
+      Console.Clear();
+      Console.WriteLine("Primeiro Valor:");
+      float firstOperator = float.Parse(Console.ReadLine());
+
+      Console.WriteLine("Segundo Valor:");
+      float secondOperator = float.Parse(Console.ReadLine());
+
+      Console.Clear();
+      float result = firstOperator / secondOperator;
+      Console.WriteLine($"O resultado da soma é: {result}");
+      Console.ReadKey();
     }
   }
 }
